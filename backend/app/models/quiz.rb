096f90e59a1ab6fc601e0332705b3b25ba492a2a
thedanielleellis <thedanielleellis@gmail.com>
2021-02-04 01:23:21 -0600

    complete set up of backend

--- backend/app/models/quiz.rb
+++ backend/app/models/quiz.rb
@@ -1,2 +1,3 @@
 class Quiz < ApplicationRecord
+    belongs_to :user
 end
--- backend/db/migrate/20210204051105_create_quizzes.rb
+++ backend/db/migrate/20210204051105_create_quizzes.rb
@@ -1,7 +1,7 @@
 class CreateQuizzes < ActiveRecord::Migration[6.0]
   def change
     create_table :quizzes do |t|
-
+    t.belongs_to :user
       t.timestamps
     end
   end
