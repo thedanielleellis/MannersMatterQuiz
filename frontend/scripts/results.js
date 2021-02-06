2e3f4ba1a964501a0e152f9e4aa7106b45fcb4fe
thedanielleellis <thedanielleellis@gmail.com>
2021-02-05 22:34:59 -0600

    created js results page

---
+++ frontend/scripts/results.js
@@ -0,0 +1,10 @@
+const username = document.querySelector('#username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+
+finalScore.innerText = mostRecentScore
+
+
+
+
